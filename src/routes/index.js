de5c20d4b39a2c859e665684391ea7f328f96beb
Sebastian Rey <reysebastian77@gmail.com>
2024-10-17 17:22:41 -0500

    actualizacion de rutas y presentacion documentacion en codigo: asignaciones

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,10 +4,10 @@ import userController from '../controllers/userController';
 const router = Router();
 
 // Users
-router.get('/users/', userController.getUsers);       // Obtener todos los usuarios
-router.get('/users/:id', userController.getUser);    // Obtener un usuario por ID
-router.post('/users/', userController.createUser);    // Crear un nuevo usuario
-router.put('/users/:id', userController.updateUser); // Actualizar un usuario existente
-router.delete('/users/:id', userController.deleteUser); // Eliminar un usuario
+router.get('/users/', userController.getUsers);          // Obtener todos los usuarios
+router.get('/users/:id', userController.getUser);        // Obtener un usuario por ID
+router.post('/users/', userController.createUser);       // Crear un nuevo usuario
+router.put('/users/:id', userController.updateUser);     // Actualizar un usuario existente
+router.delete('/users/:id', userController.deleteUser);  // Eliminar un usuario
 
 export default router;
